1f896da65e5961e158ef9cdbf2ef4cb04351a34d
j10c <blyb1739@gmail.com>
2023-07-10 20:44:23 +0800

    feat(i18n): add i18n constants and hook

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,8 +1,27 @@
-export const languages = {
-  "zh-cn": "中文",
-  "en": "English"
+import { Languages } from "@types/i18n";
+
+export const languages: {[key in Languages]: string} = {
+  [Languages["zh-cn"]]: "中文",
+  [Languages["en"]]: "English"
 }
 
-export const defaultLang = "zh-cn"
+export const defaultLang = Languages["zh-cn"]
 
-export const ui = {}
+export const ui: {[key in Languages]: { [key: string]: string }} = {
+  [Languages["zh-cn"]]: {
+    "identify.name": "J10c",
+    "identify.introduction": "前端开发",
+    "identify.occupation": "在杭大学生",
+    "identify.hobby": "热爱新事物",
+    "social.juejin": "稀土掘金",
+    "social.github": "Github"
+  }, 
+  [Languages["en"]]: {
+    "identify.name": "J10c",
+    "identify.introduction": "Front-end Developer.",
+    "identify.occupation": "A University Student in Hangzhou.",
+    "identify.hobby": "I'm fond of new things.",
+    "social.juejin": "Juejin",
+    "social.github": "Github"
+  }
+}
