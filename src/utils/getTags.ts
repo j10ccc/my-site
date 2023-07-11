04d27e30801b5ef62bba21669ff61ce13048d793
j10c <blyb1739@gmail.com>
2023-07-11 11:24:48 +0800

    perf(tag): adjust index page style
    fix(tag): handle caption different case

--- src/utils/getTags.ts
+++ src/utils/getTags.ts
@@ -1,12 +1,18 @@
 import type { Post } from "@interfaces/post";
 
+/**
+ * Get all unique tags.
+ * 
+ * @param allPosts 
+ * @returns uppercase tags
+ */
 const getTags = (allPosts: Post[]) => {
   const postsWithTag = allPosts.filter(post =>
     post.meta.tags
   );
 
   const uniqueTags = [...new Set(postsWithTag.map(post => {
-    return post.meta.tags!
+    return post.meta.tags!.map(item => item.toUpperCase());
   }).flat())];
 
   return uniqueTags;
